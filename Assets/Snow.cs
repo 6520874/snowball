3bdf60ddf7b48a8c2a1dd5a01c7b84ad520de2bb
单鹏飞 <[shanpengfei@zaizaicorp.com](mailto:shanpengfei@zaizaicorp.com)>
2022-10-22 11:05:05 +0800

    初始化逻辑

--- Assets/Snow.cs
+++ Assets/Snow.cs
@@ -1,33 +1,55 @@
 using System.Runtime.CompilerServices;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using VSCodeEditor;
 
 public class Snow : MonoBehaviour
 {
-    // Start is called before the first frame update
 
     public RenderTexture rt;
 
     public Texture drawImg;
 
+    public Camera mainCam;
+    
+
     void Start()
     {
-
+        mainCam = Camera.main.GetComponent<Camera>();
+        GetComponent<Renderer>().material.mainTexture = rt;
+    }
+    
+    public void Draw(int x,int y){
+        
         RenderTexture.active = rt;
         GL.PushMatrix();
-        GL.LoadPixelMatrix(0,0,rt.width,rt.height);
+        GL.LoadPixelMatrix(0,rt.width,rt.height,0);
         Rect rect = new Rect(0, 0, drawImg.width, drawImg.height);
-
+        
         Graphics.DrawTexture(rect,drawImg);
         GL.PopMatrix();
-
+        RenderTexture.active = null;
     }
+ 
 
-    // Update is called once per frame
     void Update()
     {
-        
-        
+        if(Input.GetMouseButton(0))
+        {
+            Debug.Log("按下");
+            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("点击到"+hit.transform.name);
+                int x = (int)(hit.textureCoord.x * rt.width);
+                int y  = (int)(hit.textureCoord.y * rt.height);
+                Draw(x,y);
+            }
+
+        }
+
     }
 }
